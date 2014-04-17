914d0b2eb4e9f437b2f57f8b2bf427087596b7cd
Matt Cox <mattcoxpdx@gmail.com>
2014-04-17 12:35:06 -0700

    add default image

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,7 +1,8 @@
 class Property < ActiveRecord::Base
   belongs_to :user, :foreign_key => :owner_id
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "150x150>"}, :default_url => "/images/:style/missing.jpg"
+  has_attached_file :image, :styles => { :medium => "300>x300", :thumb => "150>x150"}, :default_url => ActionController::Base.helpers.asset_path("assets/missing_:style.jpg"), :whiny => true
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
 
   def owner
     self.user
